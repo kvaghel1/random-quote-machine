45eddab198a15e333bf56d45141232b8aee4c5af
kvaghel1 <kvaghel1@binghamton.edu>
2017-05-18 22:00:20 -0400

    Quote JSON complete.

--- extract_quotes.py
+++ extract_quotes.py
@@ -1,6 +1,25 @@
-from urllib.request import Request,urlopen
+from urllib.request import Request,urlopen,urlretrieve
 from bs4 import BeautifulSoup
 import requests
+#import urllib
+import os
+import shutil
+
+def get_image(web_url,file_name):
+	hdr = {'User-agent':'Mozilla/5.0'}
+	req = Request(web_url,headers = hdr)
+
+	page = urlopen(req)
+	soup = BeautifulSoup(page,"html.parser")
+
+	r = requests.get(web_url,stream=True, headers=hdr)
+	if r.status_code == 200:
+		with open(file_name, 'wb') as f:
+			r.raw.decode_content = True
+			shutil.copyfileobj(r.raw, f)
+	else:
+		print (r.status_code)
+
 
 def get_page(web_url):
 	hdr = {'User-agent':'Mozilla/5.0'}
@@ -14,9 +33,27 @@ def get_page(web_url):
 	soup_divs = soup.find('div',{'class':'m-brick grid-item boxy bqQt'})
 	#print (soup_divs)
 	
+	base_url = "https://www.brainyquote.com"
 	#a tag class = "b-qt qt_371189 oncl_q"
-	soup_quote_text = soup.findAll('a',{'title':'view quote'})
-	print (len(soup_quote_text))
+	#print (soup_divs)
+	soup_quote_text = soup_divs.findAll('a',{'class':'oncl_q'})
+
+	for quote in soup_quote_text:
+		quote_json = {}
+		
+		quote_text = quote.find('img')['alt']
+		print (quote_text)
+		
+		img_src_url = quote.find('img')['src']
+		quote_id = img_src_url.split("/")[4]+"_"+img_src_url.split("/")[5]
+		print (quote_id)
+		
+		quote_json["quote_id"] = quote_id
+		quote_json["quote_text"] = quote_text
+		quote_json["quote_img_url"] = base_url + img_src_url
+
+		print (quote_json)
+		a = input("stop")
 
 	
 
@@ -24,12 +61,13 @@ if __name__ == "__main__":
 
 	pagination_number = 1
 	while(True):
+		#https://www.brainyquote.com/quotes/topics/topic_motivational.html?vm=l
 		url = "https://www.brainyquote.com/quotes/topics/topic_motivational"+str(pagination_number)+".html?vm=l"
 		print (url)
 		get_page(url)
 		pagination_number += 1
 		print (pagination_number)
-		#a = input("Continue?")
+		a = input("Continue?")
 
 
 
