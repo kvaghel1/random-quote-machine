24e69db746f4cfc80380e7071425cbacd832cce4
kvaghel1 <kvaghel1@binghamton.edu>
2017-05-24 23:04:00 -0400

    GET/POST incorrect implementation.

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import os
-from flask import Flask,render_template,send_from_directory
+from flask import Flask,render_template,send_from_directory,request
 
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=['GET','POST'])
 def main():
-    return render_template('index.html')
+	if request.method == 'POST':
+		print "Post Method"
+		temp = request.form['quote_text']
+		print "DATA: \n",temp
+		pass
+	elif request.method == 'GET':
+		print "Get Method"
+		return render_template('index.html')
+	else:
+		print "Incorrect Method"
+		
  
 if __name__ == "__main__":
     app.run()
