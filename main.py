5d2d3502fef7f34ec9ab73a2527ad5110f52d13f
kvaghel1 <kvaghel1@binghamton.edu>
2017-06-02 22:06:04 -0400

    POST method fix

--- main.py
+++ main.py
@@ -13,13 +13,14 @@ def main():
 		print "Post Method"
 		temp = request.form['quote_text']
 		print "DATA: \n",temp
-		pass
+		return render_template('index.html')
+
 	elif request.method == 'GET':
 		print "Get Method"
 		return render_template('index.html')
+	
 	else:
-		print "Incorrect Method"
-		
+		print "Incorrect Method"		
  
 if __name__ == "__main__":
     app.run()
